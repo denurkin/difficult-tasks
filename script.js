e99dc28b0db44634aaff5e429c18aecc0ce89460
Denis Iurkin <denurkin@gmail.com>
2021-08-03 00:04:12 +0300

    New

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+let num = 266219;
+let str = num.toString();
+console.log(str);
+console.log(str [0]);
+let sum = 1;
+for (let i = 0; i<str.length; i++)
+{let res = str[i];
+sum *=res;}
+console.log (sum);
+let summa = (sum ** 3);
+console.log (summa);
+let result=summa.toString();
+console.log (result.charAt(0));
+console.log (result.charAt(1));
